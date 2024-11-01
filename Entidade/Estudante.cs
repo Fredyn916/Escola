f7c40cf3a5c471c7201772bc4d9baca6cca5ff21
Fred Marques <fredyn.curso@gmail.com>
2024-11-01 16:10:32 -0300

    Adicionar arquivos de projeto.

---
+++ Entidade/Estudante.cs
@@ -0,0 +1,10 @@
+﻿namespace Entidade
+{
+    public class Estudante
+    {
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+        public double Altura { get; set; }
+        public double Peso { get; set; }
+    }
+}
